20f123494dcee6829475ded6601eb3fb732533cb
Karunia22 <karuniasariri@gmail.com>
2026-06-10 23:41:25 +0800

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\RateLimiter;
 
 class AuthController extends Controller
 {
@@ -16,22 +17,44 @@ class AuthController extends Controller
 
     public function cekData(Request $request)
     {
-        $data = $request->validate([
+        // Semua percobaan dari IP yang sama akan dihitung
+        $key = 'login-attempts-'.$request->ip();
+
+        // Maksimal 5 percobaan gagal
+        if (RateLimiter::tooManyAttempts($key, 5)) {
+
+            $seconds = RateLimiter::availableIn($key);
+
+            return back()
+                ->withInput()
+                ->with('lockout_seconds', RateLimiter::availableIn($key))->setStatusCode(429);
+
+        }
+
+        $credentials = $request->validate([
             'name' => 'required',
             'password' => 'required',
         ], [
             'name.required' => 'Username wajib diisi',
-            'name.exists' => 'Username tidak ditemukan',
             'password.required' => 'Password tidak boleh kosong',
         ]);
-        if (Auth::attempt($data)) {
+
+        if (Auth::attempt($credentials)) {
+
+            // Reset jumlah percobaan jika login berhasil
+            RateLimiter::clear($key);
 
             $request->session()->regenerate();
 
             return redirect()->route('index');
-        } else {
-            return redirect()->back()->withErrors(['password' => 'password salah'])->withInput();
         }
+
+        // Tambah jumlah percobaan gagal
+        RateLimiter::hit($key, 60);
+
+        return back()->withErrors([
+            'password' => 'Username atau password salah.',
+        ])->withInput();
     }
 
     public function logOut(Request $request)
